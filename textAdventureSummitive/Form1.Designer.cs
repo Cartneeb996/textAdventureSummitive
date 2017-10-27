426627e4f8a582140b8162e26f35892803b2253b
Cartneeb996 <CartNeeb996@ed.amdsb.ca>
2017-10-27 13:34:46 -0400

    Add project files.

---
+++ textAdventureSummitive/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace textAdventureSummitive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.bLabel = new System.Windows.Forms.Label();
+            this.rLabel = new System.Windows.Forms.Label();
+            this.bButtonPic = new System.Windows.Forms.PictureBox();
+            this.rButtonPic = new System.Windows.Forms.PictureBox();
+            this.codeInput = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bButtonPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rButtonPic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Location = new System.Drawing.Point(13, 13);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(259, 172);
+            this.outputLabel.TabIndex = 0;
+            this.outputLabel.Text = "Press R for the red button, press b for the blue button You are lost at school wh" +
+    "at do you do?";
+            // 
+            // bLabel
+            // 
+            this.bLabel.AutoSize = true;
+            this.bLabel.Location = new System.Drawing.Point(85, 209);
+            this.bLabel.Name = "bLabel";
+            this.bLabel.Size = new System.Drawing.Size(58, 13);
+            this.bLabel.TabIndex = 3;
+            this.bLabel.Text = "Go outside";
+            // 
+            // rLabel
+            // 
+            this.rLabel.AutoSize = true;
+            this.rLabel.Location = new System.Drawing.Point(84, 245);
+            this.rLabel.Name = "rLabel";
+            this.rLabel.Size = new System.Drawing.Size(59, 13);
+            this.rLabel.TabIndex = 4;
+            this.rLabel.Text = "Kill yourself";
+            // 
+            // bButtonPic
+            // 
+            this.bButtonPic.BackgroundImage = global::textAdventureSummitive.Properties.Resources.bButton;
+            this.bButtonPic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bButtonPic.Location = new System.Drawing.Point(16, 200);
+            this.bButtonPic.Name = "bButtonPic";
+            this.bButtonPic.Size = new System.Drawing.Size(33, 30);
+            this.bButtonPic.TabIndex = 7;
+            this.bButtonPic.TabStop = false;
+            // 
+            // rButtonPic
+            // 
+            this.rButtonPic.BackgroundImage = global::textAdventureSummitive.Properties.Resources.rButton;
+            this.rButtonPic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rButtonPic.Location = new System.Drawing.Point(16, 236);
+            this.rButtonPic.Name = "rButtonPic";
+            this.rButtonPic.Size = new System.Drawing.Size(33, 30);
+            this.rButtonPic.TabIndex = 8;
+            this.rButtonPic.TabStop = false;
+            // 
+            // codeInput
+            // 
+            this.codeInput.Location = new System.Drawing.Point(87, 164);
+            this.codeInput.Name = "codeInput";
+            this.codeInput.Size = new System.Drawing.Size(100, 20);
+            this.codeInput.TabIndex = 9;
+            this.codeInput.Visible = false;
+            this.codeInput.TextChanged += new System.EventHandler(this.codeInput_TextChanged);
+            this.codeInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.codeInput_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 306);
+            this.Controls.Add(this.codeInput);
+            this.Controls.Add(this.rButtonPic);
+            this.Controls.Add(this.bButtonPic);
+            this.Controls.Add(this.rLabel);
+            this.Controls.Add(this.bLabel);
+            this.Controls.Add(this.outputLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.bButtonPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rButtonPic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label bLabel;
+        private System.Windows.Forms.Label rLabel;
+        private System.Windows.Forms.PictureBox bButtonPic;
+        private System.Windows.Forms.PictureBox rButtonPic;
+        private System.Windows.Forms.TextBox codeInput;
+    }
+}
+
---
+++ textAdventureSummitive/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace textAdventureSummitive
+{
+    public partial class Form1 : Form
+    {
+        Random rndNum = new Random();
+        int scene = 0;
+        string codeInputText;
+
+        public Form1()
+        {
+            InitializeComponent();           
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.B)
+            {
+                if (scene == 0)
+                {
+                    scene = 1;
+                }
+                else if(scene == 1)
+                {
+                    scene = 3;
+                }
+                else if(scene == 4)
+                {
+                    scene = 5;                   
+                }
+            }
+            else if(e.KeyCode == Keys.R)
+            {
+                if(scene == 0)
+                {
+                    scene = 2;
+                }
+                else if(scene == 1)
+                {
+                    scene = 4;
+                }
+                else if(scene == 4)
+                {
+                    scene = 6;
+                }
+                
+            }
+            switch (scene)
+            {
+                case 0:
+                    break;
+                case 1:
+                    outputLabel.Text = "you went outside, and the sunlight burns your pale skin.\nYou go back inside.";
+                    outputLabel.Text += "\nWhich assignment should you work on first?";
+                    rLabel.Text = "Programming";
+                    bLabel.Text = "English";
+                    break;
+                case 2:
+                    outputLabel.Text = "You died";
+                    break;
+                case 3:
+                    outputLabel.Text = "You die of boredom";
+                    break;
+                case 4:
+                    outputLabel.Text = "You can't find your computer, where will you look?";
+                    bLabel.Text = "The gym";
+                    rLabel.Text = "The computer lab";                 
+                    break;
+                case 5:
+                    outputLabel.Text = "You can't find your computer, why would you look in the gym?!\nYou miss the due date, stress then have a heart attack and die";
+                    break;
+                case 6:
+                    if (rndNum.Next(1, 11) < 9)
+                    {
+                        outputLabel.Text = "You find your computer, and work on the assignment:\nto create a program to multiply to entered numbers";
+                        outputLabel.Text += "\nType in what you want to code";
+                        outputLabel.Text += "\nFirst you have to declare a interger input variable";
+                        codeInput.Visible = true;
+                        scene = 7;
+                    }
+                    else
+                    {
+                        outputLabel.Text = "You can't find your computer, you miss the due date, stress out, then have a heart attack and die";
+                    }                   
+                    break;
+                case 7:
+                    outputLabel.Text += "\nThe code:\n" + codeInputText;
+                    break;
+                case 8:
+                    outputLabel.Text = "hmmm that does not seem to work...";
+                    scene = 7;
+                    break;
+                case 9:
+                    outputLabel.Text = "that works, now we need to make another input variable " + "\nThe code:\n" + codeInputText;
+                    break;
+                case 10:
+                    outputLabel.Text = "that works, now we need to set them to inputboxes: inputA, inputB " + "\nThe code:\n" + codeInputText;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void codeInput_TextChanged(object sender, EventArgs e)
+        {
+            codeInputText = codeInput.Text;
+            
+        }
+
+        private void codeInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                outputLabel.Text += "\n" + codeInputText;
+                if (scene == 7)
+                {
+                    if (codeInput.Text == "int input;" || codeInput.Text == "int inputNum;" || codeInput.Text == "int inputNumber;")
+                    {
+                        outputLabel.Text = "that works, now we need to make another input variable" + "\nThe code:\n" + codeInput.Text;
+                        codeInputText = codeInput.Text;
+                        scene = 9;
+                    }
+                    else
+                    {
+                        outputLabel.Text = "hmmm that does not seem to work...";
+                        scene = 7;
+                    }
+                }
+                else if(scene == 9)
+                {
+                    if (codeInput.Text == "int inputB;" || codeInput.Text == "int inputNumB;" || codeInput.Text == "int inputNumberB;")
+                    {
+                        codeInputText += "\n" + codeInput.Text;
+                        outputLabel.Text = "that works, now we need to set them to inputboxes: inputA, inputB" + "\nThe code:\n" + codeInputText; 
+                        
+                        scene = 10;
+                    }
+                    else
+                    {
+                        outputLabel.Text += "hmmm that does not seem to work...";
+                        scene = 7;
+                    }
+                }
+                else if(scene == 10)
+                {
+
+                    if(codeInput.Text == "input = inputA.Text;" || codeInput.Text == "inputNum =  inputA.Text" || codeInput.Text == "inputNumber = inputNumber.Text;")
+                    {
+                        codeInputText += "\n" + codeInput.Text;
+                        outputLabel.Text = "that works, next input" + "\nThe code:\n" + codeInputText;
+                    }
+                }
+            }
+        }
+    }
+}
